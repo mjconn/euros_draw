76919a9babd63b64a005a191b0ad610955cba1a2
Mike Connolly <mjc86@hotmail.co.uk>
2021-06-11 10:31:52 +0100

    initalize, not initialise...

--- lib/euros_draw.rb
+++ lib/euros_draw.rb
@@ -1,63 +1,26 @@
 class EurosDraw
   attr_reader :teams, :people
 
-  def initialise
-    @teams = teams
-    @people = people
-  end
-
-  def teams
-    teams = ["Italy", "Switzerland", "Turkey", "Wales", "Belgium", "Russia", "Denmark", "Finland", "Ukraine", "Netherlands", "Austria", "England", "Croatia", "Czech Republic", "Scotland", "Spain", "Poland", "Sweden", "Slovakia", "Germany", "France", "Portugal"]
-  end
-
-  def people
-    people = ["Alex", "Anita", "Arran", "Dana", "Eddie", "Ehelsan", "Emily", "Emma", "Goncalo", "Hannah", "Irina", "Juel", "Katerina", "Kelvin", "Leslie", "Mike", "Muza", "Niesoth", "Ryan C", "Ryan P", "Usna", "Yusuf"]
+  def initialize
+    @teams = ["Italy", "Switzerland", "Turkey", "Wales", "Belgium", "Russia",
+      "Denmark", "Finland", "Ukraine", "Netherlands", "Austria", "England",
+      "Croatia", "Czech Republic", "Scotland", "Spain", "Poland", "Sweden",
+      "Slovakia", "Germany", "France", "Portugal"]
+    @people = ["Alex", "Anita", "Arran", "Dana", "Eddie", "Ehelsan", "Emily",
+      "Emma", "Goncalo", "Hannah", "Irina", "Juel", "Katerina", "Kelvin",
+      "Leslie", "Mike", "Muza", "Niesoth", "Ryan C", "Ryan P", "Usna", "Yusuf"]
   end
 
   def pick_team
-    shuffled_teams = teams.shuffle
-    shuffled_people = people.shuffle
-    n = 0
-    while n < shuffled_people.length
-      p shuffled_people[n] + " has got " + shuffled_teams[n]
-      n += 1
+    while teams.length > 0 && people.length > 0
+      random_team = teams.sample
+      random_person = people.sample
+      p "#{random_person} has got #{random_team}"
+      teams.delete(random_team)
+      people.delete(random_person)
     end
   end
-
-  #   n = teams.length - 1
-  #   while teams.length > 0 && people.length > 0
-  #     random_team = teams.sample
-  #     random_person = people.sample
-  #     p "#{random_person} has got #{random_team}"
-  #     teams.delete(random_team)
-  #     people.delete(random_person)
-  #   end
-  #   p @chosen_teams
-  # end
-    # while teams.length > 0 && people.length > 0
-    #   random_team = teams.sample
-    #   random_person = people.sample
-    #   p "#{random_person} gets #{random_team}"
-    #   teams.delete("#{random_team}")
-    #   people.delete("#{random_person}")
-    # end
-
-    # teams.each { |team| chosen_teams.store(team) }
-    # people.each { |person| chosen_teams.store(person) }
-    # p chosen_teams
-    # people.each { |person| p person }
-    # n = 20
-    # while @teams.length > 0
-    #   random_team = @teams[rand(n)]
-    #   @chosen_teams.store(random_team)
-    #   @teams.delete(random_team)
-    #   n -= 1
-    #   n = rand(@teams.length - 1)
-    #   @chosen_teams.store(@team[n])
-    #   team.delete(@team[n])
-    # end
-  # end
 end
 
-e = EurosDraw.new
-e.pick_team
+euros = EurosDraw.new
+euros.pick_team
